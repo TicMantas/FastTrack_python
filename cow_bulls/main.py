a8ad8947c1c34b33ab39d2a299c8711a4f4fddf0
TicMantas <mantas.tickus1998@gmail.com>
2025-02-19 10:49:15 +0200

    Initial commit
    Signed-off-by: TicMantas <mantas.tickus1998@gmail.com>

--- cow_bulls/main.py
+++
@@ -1,7 +0,0 @@
-from cow_bulls.cow_bull import generate_field, check_bull_number, check_cows_number
-
-def main():
-    field= generate_field()
-    player_prediction = int(input('Ivesk spejimu skaiciu'))
-    print(field)
-    print(player_prediction)
